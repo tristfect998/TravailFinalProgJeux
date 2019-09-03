8d9a931f0c9fe2cc8657627991ccd45e5037c3e5
B533P-06\533 <tristan.fecteau@effecto.ca>
2019-09-03 11:40:35 -0400

    Ajout asset soldier + animations de base

---
+++ Aim Training Target/Assets/Soldier/Scripts/PlayerAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void Run()
+    {
+        anim.Play("Running");
+    }
+}
