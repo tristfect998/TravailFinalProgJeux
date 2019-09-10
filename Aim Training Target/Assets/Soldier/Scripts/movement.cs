41bc736680deac34f88de69e7955c5784f782800
B533P-15\533 <tristan.fecteau@effecto.ca>
2019-09-10 09:32:48 -0400

    Quick fix des noms bizzares et changement de localisation fichiers oli (scène et script)

--- Aim Training Target/Assets/Soldier/Scripts/movement.cs
+++ Aim Training Target/Assets/Soldier/Scripts/movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moovement : MonoBehaviour
+public class movement : MonoBehaviour
 {
     Animator anim;
     bool vAxisCrouchInUse = false;
