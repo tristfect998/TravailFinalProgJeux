f762856895e31ffa371c3880865cb05e511918c1
Pier-Gabriel <azzpit5@gmail.com>
2019-09-23 17:07:43 -0400

    Ajout d'un ColorPicker et d'une scene pour gerer les couleurs

---
+++ Aim Training Target/Assets/ColorPickerTriangle/Scripts/ChangeMaterialColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMaterialColor : MonoBehaviour {
+
+    private ColorPickerTriangle colorPicker;
+    private Material material;
+
+    void Start()
+    {
+        material = GetComponent<SkinnedMeshRenderer>().material;
+        colorPicker = GameObject.FindObjectOfType<ColorPickerTriangle>();
+        colorPicker.SetNewColor(material.color);
+    }
+
+    void Update()
+    {
+        material.color = colorPicker.TheColor;
+    }
+}
