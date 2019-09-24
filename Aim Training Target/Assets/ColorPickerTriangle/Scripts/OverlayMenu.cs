fd1c1b05a29168607543ae2d377f47e935fe5762
Pier-Gabriel Labbé <azzpit5@gmail.com>
2019-09-24 11:25:45 -0400

    Finalisation de l'interface du colorPicker
    - Ajout d'un bouton Back
    - Ajout d'un bouton dans les options pour loader le colorPicker
    - Changement des materials du soldier
    - Gerer le changement de scene
    - !! La couleur ne se sauvegarde pas, donc rien change dans le jeu

---
+++ Aim Training Target/Assets/ColorPickerTriangle/Scripts/OverlayMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverlayMenu : MonoBehaviour {
+
+    public void LoadBackOptionMenu()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+}
