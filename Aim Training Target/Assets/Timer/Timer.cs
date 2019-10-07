55920080d9417917ebb290577ddb166b9bc801dc
B533P-15\533 <tristan.fecteau@effecto.ca>
2019-10-07 17:30:52 -0400

    Timer marche (asset)

---
+++ Aim Training Target/Assets/Timer/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text TimerText;
+    float TimerTime = 0f;
+    // Use this for initialization
+    void Start()
+    {
+        TimerText = gameObject.GetComponent<Text>();
+        ApplyNewTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerTime += Time.deltaTime;
+        ApplyNewTime();
+    }
+
+    void ApplyNewTime()
+    {
+        string FormatedTime = "";
+        float minutes = Mathf.Floor(TimerTime / 60);
+        float secondes = (TimerTime % 60);
+        if (minutes > 60)
+        {
+            float hours = Mathf.Floor(minutes / 60);
+            if (hours < 10)
+            {
+                FormatedTime += "0" + hours + ":";
+            }
+            else
+            {
+                FormatedTime += hours + ":";
+            }
+        }
+        else if (minutes < 10)
+        {
+            FormatedTime += "0" + minutes + ":";
+        }
+        else
+        {
+            FormatedTime += minutes + ":";
+        }
+
+        if (secondes < 10)
+        {
+            FormatedTime += "0" + Mathf.Floor(secondes);
+        }
+        else
+        {
+            FormatedTime += Mathf.Floor(secondes);
+        }
+        TimerText.text = FormatedTime;
+    }
+}
