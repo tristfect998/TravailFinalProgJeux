8b85b0f6adbd5d9e3b9af2d8163a0319af4130e2
B533P-12\533 <loucaluc@live.fr>
2019-10-22 11:40:19 -0400

    Ajout liste d'arme

---
+++ Aim Training Target/Assets/Armes/SwitchingWeapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchingWeapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Weapon1") == 1)
+        {
+            WeaponDataBase weapon = FindObjectOfType<WeaponDataBase>();
+            weapon.RecupererArme(1);
+        }
+
+        if (Input.GetAxis("Weapon2") == 1)
+        {
+            WeaponDataBase weapon = FindObjectOfType<WeaponDataBase>();
+            weapon.RecupererArme(2);
+        }
+    }
+}
---
+++ Aim Training Target/Assets/Armes/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Weapon {
+
+    public int ID;
+    public string Name;
+    public GameObject WeaponObject;
+
+}
