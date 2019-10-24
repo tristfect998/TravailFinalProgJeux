47dbcefbd2a3b0cb4ee9dc2070f82dac7e17e7ed
Loucaluc <loucaluc@live.fr>
2019-10-24 16:15:37 -0400

    debut script pour le changement d'arme

--- Aim Training Target/Assets/Armes/SwitchingWeapon.cs
+++ Aim Training Target/Assets/Armes/SwitchingWeapon.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class SwitchingWeapon : MonoBehaviour {
 
+    private int currentWeaponId = 0;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        InstantiateWeapon(0);
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetAxis("Weapon1") == 1)
         {
-            WeaponDataBase weapon = FindObjectOfType<WeaponDataBase>();
-            weapon.RecupererArme(1);
+            if (currentWeaponId != 0)
+            {
+                DestroyCurrentHolding();
+                InstantiateWeapon(0);
+            }
         }
 
         if (Input.GetAxis("Weapon2") == 1)
         {
-            WeaponDataBase weapon = FindObjectOfType<WeaponDataBase>();
-            weapon.RecupererArme(2);
+            if (currentWeaponId != 1)
+            {
+                DestroyCurrentHolding();
+                InstantiateWeapon(1);
+            }
         }
     }
-}
+
+    void InstantiateWeapon(int idArme)
+    {
+        WeaponDataBase weaponDataBase = FindObjectOfType<WeaponDataBase>();
+        GameObject arme = weaponDataBase.RecupererArmePrefab(idArme);
+        Instantiate(arme, transform);
+        currentWeaponId = idArme;
+    }
+
+    void DestroyCurrentHolding()
+    {
+       /* GameObject currentHoldingWeapon = GetComponentInParent<GameObject>();
+        Destroy(currentHoldingWeapon); */
+    }
+
+
+    }
--- Aim Training Target/Assets/Armes/WeaponDataBase.cs
+++ Aim Training Target/Assets/Armes/WeaponDataBase.cs
@@ -6,7 +6,7 @@ public class WeaponDataBase : MonoBehaviour {
 
     public List<Weapon> Item = new List<Weapon>();
 
-    public GameObject RecupererArme(int indice)
+    public GameObject RecupererArmePrefab(int indice)
     {
         GameObject arme = null;
         if (indice < Item.Count)
